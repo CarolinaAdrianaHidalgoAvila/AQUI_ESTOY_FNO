9879de1ada9120a3ded31f813e075393e71b04f5
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-09-27 17:54:49 -0400

    Service_Pets

---
+++ BACK_END/AquiEstoy MongoDB/Services/PetService.cs
@@ -0,0 +1,27 @@
+﻿using AquiEstoy_MongoDB.Data.Entities;
+using AquiEstoy_MongoDB.Data.Repository;
+using AquiEstoy_MongoDB.Models;
+using AutoMapper;
+
+namespace AquiEstoy_MongoDB.Services
+{
+    public class PetService: IPetService
+    {
+            private IUserCollection _userCollection;
+            private IMapper _mapper;
+            public PetService(IUserCollection userCollection, IMapper mapper)
+            {
+                _userCollection = userCollection;
+                _mapper = mapper;
+            }
+
+            public async Task<IEnumerable<PetModel>> GetAllPetsAsync()
+            {
+                var petsEntityList = await _userCollection.GetAllPetsAsync();
+                var petsModelList = _mapper.Map<IEnumerable<PetModel>>(petsEntityList);
+                return petsModelList;
+            }
+
+        
+    }
+}
