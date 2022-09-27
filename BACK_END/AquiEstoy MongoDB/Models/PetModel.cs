fc105a9f229d95d60bf8cd1318f34ee69e4b55a4
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-09-27 17:38:19 -0400

    Model_Pets

---
+++ BACK_END/AquiEstoy MongoDB/Models/PetModel.cs
@@ -0,0 +1,20 @@
+﻿using AquiEstoy_MongoDB.Data.Entities;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace AquiEstoy_MongoDB.Models
+{
+    public class PetModel
+    {
+        public ObjectId IdPet { get; set; }
+
+        public string? NamePet { get; set; }
+        public DateOnly? BirthDate { get; set; }
+
+        public string? Gender { get; set; }
+        public bool? HasNecklace { get; set; }
+        public string? Specie { get; set; }
+        public UserEntity? UserID { get; set; }
+    }
+}
