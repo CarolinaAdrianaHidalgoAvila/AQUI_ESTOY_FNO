728e66cb323897fae1c3f9d179c8f5ef60841835
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-09-27 16:58:18 -0400

    Controller_Pets

---
+++ BACK_END/AquiEstoy MongoDB/Controllers/PetsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AquiEstoy_MongoDB.Exceptions;
+using AquiEstoy_MongoDB.Models;
+using AquiEstoy_MongoDB.Services;
+using System.Threading.Tasks;
+
+namespace AquiEstoy_MongoDB.Controllers
+{
+    [Route("users/{userId:int}/[controller]")]
+    public class PetsController : ControllerBase
+    {
+        private IPetsService _petService;
+
+        public PetsController(IPetsService petService)
+        {
+            _petService = petService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PetModel>>> GetPets(int userId)
+        {
+            try
+            {
+                return Ok(await _petService.GetPetsAsync(userId));
+            }
+            catch (NotFoundOperationException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Something happend: {ex.Message}");
+            }
+        }
+    }
+}
