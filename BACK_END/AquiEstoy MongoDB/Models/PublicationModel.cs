6d664e09726402c511e5b536643e81a9151242ca
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2022-10-25 09:58:43 -0400

    Model Publication

--- BACK_END/AquiEstoy MongoDB/Models/PublicationModel.cs
+++ BACK_END/AquiEstoy MongoDB/Models/PublicationModel.cs
@@ -2,5 +2,14 @@
 {
     public class PublicationModel
     {
+        public string IdPublication { get; set; } = string.Empty;
+
+        public string? NamePet { get; set; }
+        public string? Species { get; set; }
+        public string? DatePublication { get; set; }
+        public string? Location { get; set; }
+        public string? Email { get; set; }
+        public string? Description { get; set; }
+        public string? UserID { get; set; }
     }
 }
