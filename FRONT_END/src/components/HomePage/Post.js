d473a837d1c2e6092c0487ecb00ac55d46e64b9b
hmuyba <50967848+hmuyba@users.noreply.github.com>
2022-11-10 03:15:11 -0400

    feed

---
+++ FRONT_END/src/components/HomePage/Post.js
@@ -0,0 +1,63 @@
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Checkbox,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+
+const Post = () => {
+  return (
+    <Card sx={{margin:5}}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="John "
+        subheader="September 14, 2022"
+      />
+      <CardMedia
+        component="img"
+        height="20%"
+        image="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt="cat"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <Checkbox
+            icon={<FavoriteBorderIcon />}
+            checkedIcon={<FavoriteIcon sx={{ color: "red" }} />}
+          />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Post;
--- FRONT_END/src/components/HomePage/Sidebar.js
+++ FRONT_END/src/components/HomePage/Sidebar.js
@@ -1,70 +1,71 @@
-import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
-import React from 'react'
-import HomeIcon from '@mui/icons-material/Home';
-import PetsIcon from '@mui/icons-material/Pets';
-import ArticleIcon from '@mui/icons-material/Article';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import PetsIcon from "@mui/icons-material/Pets";
+import ArticleIcon from "@mui/icons-material/Article";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
 
 const Sidebar = () => {
   return (
-    <Box flex={1}  p={2} sx={{display:{xs:'none' , sm:'block'}}}>
-
-
+    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position='fixed'>
         <List>
-            <ListItem disablePadding>
-                <ListItemButton component='a' href='/'>
-                <ListItemIcon>
-                    <HomeIcon/>
-                </ListItemIcon>
-                <ListItemText primary="Pagina principal" />
-                </ListItemButton>
-            </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/">
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Pagina principal" />
+            </ListItemButton>
+          </ListItem>
 
-            <ListItem disablePadding>
-                <ListItemButton component='a' href='/'>
-                <ListItemIcon>
-                    <PetsIcon/>
-                </ListItemIcon>
-                <ListItemText primary="Tus Mascotas" />
-                </ListItemButton>
-            </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/">
+              <ListItemIcon>
+                <PetsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Tus Mascotas" />
+            </ListItemButton>
+          </ListItem>
 
-            <ListItem disablePadding>
-                <ListItemButton component='a' href='/'>
-                <ListItemIcon>
-                    <ArticleIcon/>
-                </ListItemIcon>
-                <ListItemText primary="Tus publicaciones" />
-                </ListItemButton>
-            </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/">
+              <ListItemIcon>
+                <ArticleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Tus publicaciones" />
+            </ListItemButton>
+          </ListItem>
 
-            <ListItem disablePadding>
-                <ListItemButton component='a' href='/'>
-                <ListItemIcon>
-                    <BarChartIcon/>
-                </ListItemIcon>
-                <ListItemText primary="Estadisticas" />
-                </ListItemButton>
-            </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/">
+              <ListItemIcon>
+                <BarChartIcon />
+              </ListItemIcon>
+              <ListItemText primary="Estadisticas" />
+            </ListItemButton>
+          </ListItem>
 
-            <ListItem disablePadding>
-                <ListItemButton component='a' href='/user'>
-                <ListItemIcon>
-                    <AccountBoxIcon/>
-                </ListItemIcon>
-                <ListItemText primary="Perfil" />
-                </ListItemButton>
-            </ListItem>
-
-
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/user">
+              <ListItemIcon>
+                <AccountBoxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Perfil" />
+            </ListItemButton>
+          </ListItem>
         </List>
-
-
-
+      </Box>
     </Box>
+  );
+};
 
-  )
-}
-
-export default Sidebar
+export default Sidebar;
