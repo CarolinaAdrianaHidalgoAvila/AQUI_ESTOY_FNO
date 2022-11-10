1a5bb5f52194ff7d82799025a7c1187bd8c53289
hmuyba <50967848+hmuyba@users.noreply.github.com>
2022-11-10 02:08:51 -0400

    sideBar

--- FRONT_END/src/pages/HomePage.jsx
+++ FRONT_END/src/pages/HomePage.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import Navbar from '../components/NavBar/Navbar'
 import MediaCard from '../components/MediaCard/MediaCard';
+import Sidebar from '../components/HomePage/Sidebar';
+import Feed from '../components/HomePage/Feed';
+import Rightbar from '../components/HomePage/Rightbar';
+import { Box, Stack } from '@mui/material';
 
 function HomePage() {
   return (
     <>
       <div  style={{marginTop: '3em'}} className="homePage">
-        <MediaCard/>
+        <Box>
+          <Stack direction='row' spacing={2} justifyContent='space-between' >
+            <Sidebar/>
+            <Feed/>
+            <Rightbar/>
+
+          </Stack>
+        </Box>
       </div>
     </>
   )
